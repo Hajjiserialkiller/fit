7c9f603b0c81977d89111365c9422633c7a6e493
Hajjiserialkiller <fbidea4b@gmail.com>
2023-10-21 18:03:15 +0100

    Initial commit for fit folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Hero from "./compenents/Hero/Hero";
+import Programs from "./compenents/Programs/Programs";
+import Reasons from "./compenents/Reasons/Reasons";
+import Plans from "./compenents/Plans/Plans";
+import Testimonials from "./compenents/Testimonials/Testimonials";
+import Join from "./compenents/Join/Join";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero />
+      <Programs />
+      <Reasons />
+      <Plans />
+      <Testimonials />
+      <Join />
     </div>
   );
 }
